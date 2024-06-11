03cb542334ede9d1a121e9c7855982f53eb7f257
MacBook Pro Sandrine CP <macbookprosandrinecp@MacBook-Pro-de-MacBook.local>
2024-06-11 16:59:29 +0200

    changes on PokemonCard.jsx to add condition

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,26 @@
+const pokemonlist = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard () {
+    const pokemon = pokemonlist[0];
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokémon Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    )
+       <figure>
+        {pokemon.imgSrc ? (
+            <img src={pokemon.imgSrc} alt={`Pokemon ${pokemon.name}`} />
+        ) : (
+            <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+       </figure>
+    );
 }
+
 
 export default PokemonCard
