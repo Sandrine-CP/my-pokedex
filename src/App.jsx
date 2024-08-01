897bc5d8d3d81c01f97132247adb1b392a9e00f1
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2024-08-01 12:46:06 +0200

    events challenge done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
+import "./App.css";
 
 function App() {
+  
   const [pokemonIndex, setPokemonIndex] = useState(0);
-
+  
   const pokemonList = [
     {
       name: "bulbasaur",
       imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
       name: "charmander",
       imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
     {
       name: "squirtle",
       imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
     },
     {
       name: "pikachu",
       imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
       name: "mew",
     },
   ];
+  
+  useEffect(
+    () => {
+      alert("Hello pokemon trainer :)");
+    },[]);
 
   return (
     <div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 
 function NavBar({ pokemonList, setPokemonIndex }) {
+  const handleClick = (pokemon, index) => {
+    setPokemonIndex(index);
+    if (pokemon.name === "pikachu") {
+      alert("pika pikachu !!!");
+    }
+  }
   return (
     <div>
       {pokemonList.map((pokemon, index) => (
-        <button key={pokemon.name} onClick={() => setPokemonIndex(index)}>
+        <button key={pokemon.name} onClick={() => handleClick(pokemon,index)}>
           {pokemon.name}
         </button>
       ))}
